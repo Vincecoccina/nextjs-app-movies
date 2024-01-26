2f266c9f971ed25b428233948f07bd7acc9c6739
Vince <v.coccina@orange.fr>
2024-01-26 10:53:32 +0100

    comments section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,18 +16,6 @@ export default function Footer() {
                 3EQppuiJWYgokwrgGCL38ZNHNj9QgowE2G
               </span>{" "}
             </p>
-            <p>
-              Don Ethereum :{" "}
-              <span className="text-slate-800 dark:text-orange-300">
-                0x2bF90C283aC155734d57Aa7338eC9E287d2f0CEf
-              </span>{" "}
-            </p>
-            <p>
-              Don Matic :{" "}
-              <span className="text-slate-800 dark:text-orange-300">
-                0x2bF90C283aC155734d57Aa7338eC9E287d2f0CEf
-              </span>{" "}
-            </p>
           </div>
           <a href="/" className="bg-green-500 rounded-full py-1 px-3">
             <Image
